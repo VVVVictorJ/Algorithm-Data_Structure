444385b76888da931f2a45537c38745606054896
VVVVictorJ <956951190@qq.com>
2018-04-30 14:02:57 +0800

    iteration train double for DSA

---
+++ 迭代/迭代.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void swap(int &a,int &b);
+int* reverse(int* A, int lo, int hi);
+void onsreen(int* A);
+void onsreen(int* A) 
+{
+	for (int i=0; i < 8; i++) {
+		printf("%d\t",A[i]);
+	}
+	printf("\n");
+}
+int* reverse(int* A, int lo, int hi) 
+{
+	if (lo < hi) {
+		swap(A[lo], A[hi]);
+		onsreen(A);
+		reverse(A, lo + 1, hi - 1);
+		
+	}
+	return	A;
+}
+void swap(int &a, int &b)
+{
+	int c = 0;
+	c = b;
+	b = a;
+	a = c;
+	//Ç³¿½±´Éî¿½±´
+}
+int main()
+{
+	int a[8] = { 1,2,3,4,5,6,7,8 };
+	reverse(a, 0, 7);
+	onsreen(a);
+	getchar();
+}
